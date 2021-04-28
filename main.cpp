1e788a74f9aff174312d33754755f1131286bd0d
Marcel Oliveira <ligimixi@gmail.com>
2021-04-28 11:15:52 +0200

    updated with master

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
 int main() {
+
     int ia[] = {1,2,3,4,5,6,7,8,9,10};
 
     std::cout << "Hallo ich bin Felix\n";
     std::cout << "Hallo ich bin Felix\n";
-        std::cout << "Hallo ich bin Felix\n";
-            std::cout << "Hallo ich bin Felix\n";
-                std::cout << "Hallo ich bin Felix\n";
-                    std::cout << "Hallo ich bin Felix\n";
-                        std::cout << "Hallo ich bin Felix\n";
-                            std::cout << "Hallo ich bin Felix\n";
-                            
+    std::cout << "Hallo ich bin Felix\n";
+    std::cout << "Hallo ich bin Felix\n";
+    std::cout << "Hallo ich bin Felix\n";
+    std::cout << "Hallo ich bin Felix\n";
+    std::cout << "Hallo ich bin Felix\n";
+    std::cout << "Hallo ich bin Felix\n";       
 
 
     return EXIT_SUCCESS;
